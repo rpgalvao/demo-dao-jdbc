0d4abbed690671906824c0935edfbf18a9ccfb77
Renato Galvao <renatopgalvao@gmail.com>
2019-12-31 06:53:00 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(25, "Renato Galvao", "renato@mail.com", new Date(), 5000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 }
